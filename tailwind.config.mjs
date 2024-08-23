1c45b3ffb117ffa95b290a225bdff2c50c587df1
Marcelo del Rio <mrio@unifiedhealth.com>
2024-08-23 09:22:02 -0400

    feat: add layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,27 +5,42 @@ import colors from "tailwindcss/colors";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        lg: '0rem',
+      },
+      screens: {
+        sm: "100%",
+        md: "64rem",
+      },
+    },
     colors: {
       ...colors,
       primary: {
-        light: "#6b7280",
-        DEFAULT: "#13151a",
-        title: "#1e3a8a",
-        subtitle: "#6b7280",
-        accent: "#9C27B0",
-        piper: "#1d2432",
+        light: "#FFBD39",
+        dark: "#FFBD39",
       },
-      secondary: {
-        light: "#f3f4f6",
-        DEFAULT: "#9C9C9C",
-        dark: "#d1d5db",
+      background: {
+        light: "#f5f9fc",
+        dark: "#000000",
+      },
+      paper: {
+        light: "#ebebeb",
+        dark: "#080808",
+      },
+      color: {
+        light: "#3f3f46",
+        dark: "#d4d4d8",
       },
     },
     extend: {
       fontFamily: {
         sans: ["IBM Plex Mono", ...defaultTheme.fontFamily.sans],
-        serif: ["Raleway Variable", ...defaultTheme.fontFamily.serif],
+        serif: ["Raleway Vaablrie", ...defaultTheme.fontFamily.serif],
       },
     },
   },
