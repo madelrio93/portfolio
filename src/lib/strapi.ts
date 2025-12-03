75b202f4067f32fc253545ef2a4c329d2e49dd88
Marcelo del Rio <madelrio93@gmail.com>
2025-12-03 18:18:15 -0500

    Integrates dynamic social profile data from Strapi CMS
    Fetches social profile links and homepage content from Strapi,
    replacing hardcoded values with dynamic data passed via props.
    Improves flexibility and maintainability by centralizing profile
    data management and enabling content updates without code changes.

---
+++ src/lib/strapi.ts
@@ -0,0 +1,43 @@
+const STRAPI_API_URL = import.meta.env.STRAPI_API_URL;
+
+const getHomePageData = async () => {
+  const data = await getStrapiData(
+    "home-page?populate[sections][on][layout.hero-section][populate]"
+  );
+
+  return data;
+};
+
+const getSocialProfilesData = async () => {
+  const result = await getStrapiData("social-link");
+
+  if (!result)
+    return {
+      linkedinUrl: "#",
+      githubUrl: "#",
+      emailUrl: "#",
+    };
+
+  return {
+    linkedinUrl: result.data?.linkedinUrl,
+    githubUrl: result.data?.githubUrl,
+    emailUrl: result.data?.emailUrl,
+  };
+};
+
+const getStrapiData = async (endpoint: string) => {
+  try {
+    console.log(`${STRAPI_API_URL}/${endpoint}`);
+    const response = await fetch(`${STRAPI_API_URL}/${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+export { getHomePageData, getSocialProfilesData };
